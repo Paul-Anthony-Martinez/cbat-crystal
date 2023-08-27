95c6795973c1b39b712f6551f5eb0aaefb3a1384
Paul-Anthony-Martinez <58192822+Paul-Anthony-Martinez@users.noreply.github.com>
2023-08-27 04:21:11 -0500

    Update cb.cr
    Removed redundant comments

--- cb.cr
+++ cb.cr
@@ -6,7 +6,6 @@ require "dir"
 def list_batteries(supply_dir)
    files = Dir.children(supply_dir)
    files_list = Array(String).new
-
    files.each do |line|
       if /BAT*/.match(line)
          files_list << line.to_s
@@ -19,13 +18,11 @@ end
 def cat_path(supply_dir, files_list)
    cap_paths = Array(String).new
    stat_paths = Array(String).new
-
    # when iterating, line contains the name of the present element in the array
    files_list.each do |line|
       cap_paths << supply_dir + line + "/capacity"
       stat_paths << supply_dir + line + "/status"
    end
-
    return cap_paths, stat_paths
 end
 
@@ -33,7 +30,6 @@ end
 def get_values(supply_dir, files_list, cap_paths, stat_paths)
    bat_capacity = Array(Int32).new()
    bat_status = Array(String).new()
-
    i = 0
    files_list.each do |bat|
       capacity = 0
@@ -46,7 +42,6 @@ def get_values(supply_dir, files_list, cap_paths, stat_paths)
       file = File.open(stat_path) do |file|
          status = file.gets_to_end()
       end
-
       bat_capacity << capacity.to_i()
       bat_status << status.to_s()
       i += 1
@@ -54,30 +49,25 @@ def get_values(supply_dir, files_list, cap_paths, stat_paths)
    return bat_capacity, bat_status
 end
 
-# this method stores the current capacity bar in an array to be displayed
 # when iterating to each battery we calculate floor of the capacity times the amount of spaces for the bar divided by a 100
 # each bar is stored in a string and then, when moving to the next battery, the current string is stored in the array.
-# the bar is drawn by first drawing the amount of bars representing the current capacity, then 
-# the rest by using the same variable in the next while loop.
+# the bar is drawn by first drawing the amount of bars representing the current capacity, then the rest by using the same variable in the next while loop.
 def draw_bar(spaces, files_list, bat_capacity)
    i = 0
    bat_bars = Array(String).new()
    files_list.each do |line|
       capacity = bat_capacity[i].to_i()
       floor = (capacity * spaces/ 100).round()
-      bar = ""
-      
+      bar = "" 
       b = 0
       while b < floor
          bar += "\u2588"
          b += 1
       end
-
       while b < spaces
          bar += "\u2591"
          b += 1
       end
-
       bat_bars << bar
       i += 1
    end
@@ -90,12 +80,10 @@ def display_data(files_list, bat_status, bat_capacity, bat_bars)
    files_list.each do |line|
       print "<#{line}>"
       print "\t[#{bat_bars[i].strip()}] [#{bat_capacity[i]}%] [#{bat_status[i].strip()}]"
-
       i += 1
    end
    print "\n"
 end
-
 
 spaces = 20
 supply_dir = "/sys/class/power_supply/"
