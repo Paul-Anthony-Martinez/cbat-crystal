b1943e0a220d67a99f3853b76540b37f8f670062
anthonyslx <pablo17pamg@duck.com>
2024-04-15 21:56:46 -0500

    Improved writting of --help option.

--- src/res/info.cr
+++ src/res/info.cr
@@ -1,7 +1,16 @@
 struct Information
-   property description = "[CBAT] A minimal Crystal program to show your battery status."
+   property description = "Usage: cbat [OPTION]...\
+   \nA minimal Crystal program to show your battery status.\
+   \nWithout arguments, output will be: [%n][%p][%b][%c]\
+   \n\nOptions:"
    property width = "Sets the amount of space for the bar to display."
+   property color = "Enables color in the bar output."
+   property format_str = "Receives a string that specifies how information will \
+   \nbe returned. Variables are specified with a % and a letter:\
+   \n\t'%b' for the bar\t'%p' for percentage\
+   \n\t'%n' for battery name\t'%s' for charging status."
    property help = "Display this help menu."
-   property version = "Display the current installed version of CBAT."
-   property credits = "[CBAT-Version] 0.5\n[Compiled] 4-2-2024\n[Credits] Pablo Martínez & Emanuel Avilés.\n"
+   property version = "Display the current installed version of CBat."
+   property credits = "CBAT 0.1 (13.4.2024)\n\nProgrammed by \
+   Pablo Martínez & Emanuel Avilés.\n"
 end
